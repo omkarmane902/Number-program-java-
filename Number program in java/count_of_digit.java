5e7a73ca77c0edf2918c37dd343437540601337a
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-08-22 23:31:09 +0530

    Add files via upload

---
+++ Number program in java/count_of_digit.java
@@ -0,0 +1,36 @@
+package number_program_all;
+
+import java.util.Scanner;
+
+public class count_of_digit {
+
+	
+	public static void main(String[] args) {
+	
+		
+	 Scanner sc = new Scanner(System.in);
+	 
+	 System.out.println("Enter the number");
+	 
+	 
+	 int num = sc.nextInt();
+	 
+	 int count = 0;
+	 
+	 while( num > 0)
+	 {
+		num = num / 10;
+		
+		count ++;
+		
+		 
+	 }
+	 
+	 
+	 System.out.println("Count Number is : "+count);
+	  sc.close();
+	  
+	  
+	  
+	}
+}
